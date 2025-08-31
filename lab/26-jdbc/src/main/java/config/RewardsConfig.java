316c12cc950542b16a76370a734c90e54560bca7
jp3pe <45834750+jp3pe@users.noreply.github.com>
2025-08-31 20:50:11 +0900

    Finished section 26 Spring JDBC (#5)

--- lab/26-jdbc/src/main/java/config/RewardsConfig.java
+++ lab/26-jdbc/src/main/java/config/RewardsConfig.java
@@ -1,11 +1,9 @@
 package config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
 import rewards.internal.account.AccountRepository;
@@ -15,36 +13,37 @@ import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.JdbcRewardRepository;
 import rewards.internal.reward.RewardRepository;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class RewardsConfig {
 
-	@Autowired
-	DataSource dataSource;
-		
-	@Bean
-	public RewardNetwork rewardNetwork(){
-		return new RewardNetworkImpl(
-			accountRepository(), 
-			restaurantRepository(), 
-			rewardRepository());
-	}
-	
-	@Bean
-	public AccountRepository accountRepository(){
-		JdbcAccountRepository repository = new JdbcAccountRepository(dataSource);
-		return repository;
-	}
-	
-	@Bean
-	public RestaurantRepository restaurantRepository(){
-		JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
-		return repository;
-	}
-	
-	@Bean
-	public RewardRepository rewardRepository(){
-		JdbcRewardRepository repository = new JdbcRewardRepository(dataSource);
-		return repository;
-	}
-	
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public RewardNetwork rewardNetwork() {
+        return new RewardNetworkImpl(
+                accountRepository(),
+                restaurantRepository(),
+                rewardRepository());
+    }
+
+    @Bean
+    public AccountRepository accountRepository() {
+        JdbcAccountRepository repository = new JdbcAccountRepository(new JdbcTemplate(dataSource));
+        return repository;
+    }
+
+    @Bean
+    public RestaurantRepository restaurantRepository() {
+        JdbcRestaurantRepository repository = new JdbcRestaurantRepository(new JdbcTemplate(dataSource));
+        return repository;
+    }
+
+    @Bean
+    public RewardRepository rewardRepository() {
+        JdbcRewardRepository repository = new JdbcRewardRepository(new JdbcTemplate(dataSource));
+        return repository;
+    }
 }
