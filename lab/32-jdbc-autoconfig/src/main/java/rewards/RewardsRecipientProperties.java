ab71bdb3bff897dc70b4a75098dc283c3bb0b5c5
jp3pe <45834750+jp3pe@users.noreply.github.com>
2025-11-09 19:59:50 +0900

    Finish unit 32 (#7)

--- lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsRecipientProperties.java
+++ lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsRecipientProperties.java
@@ -1,24 +1,52 @@
 package rewards;
 
-// TODO-06 : Capture properties into a class using @ConfigurationProperties
-// - Note that application.properties file already contains the following properties
-//
-//    rewards.recipient.name=John Doe
-//    rewards.recipient.age=10
-//    rewards.recipient.gender=Male
-//    rewards.recipient.hobby=Tennis
-//
-// - Annotate this class with @ConfigurationProperties
-//   with prefix attribute set to a proper value
-// - Create fields (along with needed getters/setters) that reflect the
-//   properties above in the RewardsRecipientProperties class
-// - Use one of the following 3 schemes to enable @ConfigurationProperties
-//   (1) Add @EnableConfigurationProperties(RewardsRecipientProperties.class)
-//       to the RewardsApplication class
-//   (2) Add @ConfigurationPropertiesScan to RewardsApplication class or
-//   (3) Annotate this class with @Component
-// - Implement a new command line runner that displays the name of the rewards
-//   recipient when the application gets started
-public class RewardsRecipientProperties {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
+@ConfigurationProperties(prefix = "rewards.recipient")
+public class RewardsRecipientProperties {
+    private String name;
+    private String age;
+    private String gender;
+    private String hobby;
+
+    @Bean
+    CommandLineRunner rewardsRecipientPropertiesRunner(RewardsRecipientProperties rewardsRecipientProperties) {
+        return args -> {
+            System.out.println("rewards-recipient name: " + rewardsRecipientProperties.getName());
+        };
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
 }
