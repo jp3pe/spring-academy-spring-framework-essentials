d9281915c5bd0ca606b261e6d124278676e32005
jp3pe <45834750+jp3pe@users.noreply.github.com>
2026-01-24 16:55:40 +0900

    Finish actuator (#13)
    * Finish until TODO-04
    
    * Finish until TODO-07
    
    * Finish until TODO-11
    
    * Finish until TODO-13
    
    * Finish until TODO-14
    
    * Finish until TODO-19
    
    * Finish until TODO-24
    
    * Finish until TODO-25
    
    * 코드 정렬
    
    * Finish until TODO-26
    
    * Finish until TODO-28

--- lab/44-actuator/src/test/java/accounts/client/AccountClientSecurityTests.java
+++ lab/44-actuator/src/test/java/accounts/client/AccountClientSecurityTests.java
@@ -1,6 +1,5 @@
 package accounts.client;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,20 +8,14 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-// TODO-24: Run the tests checking security configuration for the actuator endpoints
-// - Take some time to understand what each test is for
-// - Remove @Disabled annotation from each test and run it
-// - Make sure all tests pass
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AccountClientSecurityTests {
-
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    @Disabled
     public void any_user_can_access_health_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.getForEntity("/actuator/health", String.class);
@@ -30,7 +23,6 @@ public class AccountClientSecurityTests {
     }
 
     @Test
-    @Disabled
     public void any_user_can_access_info_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.getForEntity("/actuator/info", String.class);
@@ -38,30 +30,26 @@ public class AccountClientSecurityTests {
     }
 
     @Test
-    @Disabled
     public void any_user_cannot_access_conditions_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.withBasicAuth("anyuser", "anyuser")
-                              .getForEntity("/actuator/conditions", String.class);
+                .getForEntity("/actuator/conditions", String.class);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
 
     @Test
-    @Disabled
     public void user_in_ADMIN_role_can_access_conditions_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.withBasicAuth("admin", "admin")
-                              .getForEntity("/actuator/conditions", String.class);
+                .getForEntity("/actuator/conditions", String.class);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
-    @Disabled
     public void user_in_ACTUATOR_role_cannot_access_conditions_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.withBasicAuth("actuator", "actuator")
-                              .getForEntity("/actuator/conditions", String.class);
+                .getForEntity("/actuator/conditions", String.class);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
     }
-
 }
