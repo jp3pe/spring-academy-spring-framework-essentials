e1339359120028e4586b5aed78826861258417cc
jp3pe <rlaworhkd430430@gmail.com>
2026-01-24 14:19:55 +0900

    Finish until TODO-07

--- lab/44-actuator/src/main/java/accounts/web/AccountController.java
+++ lab/44-actuator/src/main/java/accounts/web/AccountController.java
@@ -55,6 +55,8 @@ public class AccountController {
 	 */
 	@GetMapping(value = "/accounts")
 	public List<Account> accountSummary() {
+        logger.debug("Logging message within accountSummary()");
+
 		return accountManager.getAllAccounts();
 	}
 
